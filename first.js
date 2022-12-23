e0355fdeac0242a8ead7cfcb5cba1e3c3b898a3d
seungryeol <opop0421@naver.com>
2022-12-23 13:57:28 +0900

    1차

---
+++ first.js
@@ -0,0 +1,15 @@
+"use strict";
+const a = 5;
+const b = "hello";
+const c = false;
+const a1 = 5;
+const ab = 24;
+const ar1 = ['asdf'];
+const ar2 = [23, 2]; //배열
+const ar3 = [34, 43, 34]; //제네릭
+const ar4 = [4, 5]; //튜플
+function add(x, y) { return x + y; }
+const add1 = (x, y) => x + y;
+const sub = (x, y) => x + y;
+const sub1 = (x, y) => x + y;
+const sub2 = (x, y) => x + y;
---
+++ first.ts
@@ -0,0 +1,24 @@
+const a : number = 5;
+const b : string ="hello";
+const c : boolean = false;
+const a1 : 5 = 5;
+const ab : any = 24; 
+const ar1 : string[] = ['asdf'];
+const ar2 : number[] = [23,2];          //배열
+const ar3 : Array<number> = [34,43,34]; //제네릭
+const ar4 : [number,number] = [4,5];     //튜플
+
+
+function add(x: number, y:number): number { return x+y;}     
+const add1 : (x:number, y:number) => number = (x,y) => x+y;
+
+const sub : (x:number, y:number) => number = (x,y) => x+y;
+type Add =  (x:number, y:number) => number;        //타입 에일리어스
+const sub1 : Add = (x,y) => x+y;
+
+
+
+interface de{                            //인터페이스
+    (x:number,y:number):number;
+}
+const sub2 : de = (x,y) => x+y;

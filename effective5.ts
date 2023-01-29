edff9ac20cc0c05c3c2fae7980cea0e036b44fcd
seungryeol <opop0421@naver.com>
2023-01-29 14:22:55 +0900

    39.any를 구체적으로 변형해서 사용하기

--- effective5.ts
+++ effective5.ts
@@ -52,3 +52,30 @@ const config2 : Config = {        // xxxx
     }
 };
 
+/* 39.any를 구체적으로 변형해서 사용하기 */
+
+function getLengthBad(array:any){
+    return array.length;
+}
+
+function getLength(array:any[]){                  // 함수의 반환 타입이 any대신 number로 추론됩나다.
+    return array.length;                          // 함수 호출될 때 매개변수가 배열인지 체크됩니다.
+}
+
+getLengthBad(23);
+//getLength(23);    // Error
+
+function haTwelveLetterKey(o:{[key:string]:any}){
+    for(const key in o){
+        if(key.length === 12){
+            return true;
+        }
+    }
+    return false;
+}
+
+haTwelveLetterKey({'a':23,'b':'we'});
+
+type Fn0 = () => any;                 // 매개변수 없이 호출 가능한 모든 함수
+type Fn1 = (arg:any) => any;          // 매개변수 1개
+type Fn2 = (...args:any[]) => any;    // 모든 개수의 매개변수

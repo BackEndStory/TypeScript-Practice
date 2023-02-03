b0a1f228cfd9809609e18cae20239de3401e64b4
seungryeol <opop0421@naver.com>
2023-02-03 19:38:59 +0900

    48.API주석에 TSDoc 사용하기

--- effective-typescript/effective6.ts
+++ effective-typescript/effective6.ts
@@ -21,3 +21,36 @@ export default function getGift(name:SecretName, gift: string): ScretSanta {
 };
 //export { SecretName, ScretSanta}  
 // => effective5.ts
+
+
+/* 48.API주석에 TSDoc 사용하기 */
+
+/** 
+ * 인사말을 생성합니다. 
+ * @param 인사할 사람의 이름 
+ * @param 그 사람의 칭호
+ * @returns 사람이 보기 좋은 형태의 인사말
+ */
+function greetJSDoc(name:string,title:string){
+    return `Hello ${title}`;
+}
+
+/** 
+ * 이 _interface_는 **세 가지** 속성을 가집니다.
+ * 1. x
+ * 2. y
+ * 3. z
+ */
+interface Vector3D{
+    x:number;
+    y:number;
+    z:number;
+}
+
+/** 특정 시간과 장소에서 수행된 측정 */
+interface Measurement{
+    /** 어디에서 측정되었나? */
+    position:Vector3D;
+    /** 언제 측정되었나? epoch에서부터 초 단위로 */
+    time:number;
+}

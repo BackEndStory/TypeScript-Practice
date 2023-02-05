de9b86a6b9e570c2f44f0db00b442f121c18e77e
seungryeol <opop0421@naver.com>
2023-02-05 13:34:46 +0900

    54.객체를 순회하는 노하우

--- effective-typescript/effective7.ts
+++ effective-typescript/effective7.ts
@@ -62,3 +62,40 @@ console.log(new Greeter('Dave').greet());
 
 
 
+/* 54.객체를 순회하는 노하우 */
+
+const obj3 = {
+  one:'uno',
+  two:'dos',
+  three:'tres'
+};
+let k : keyof typeof obj3;
+for(k in obj3){
+ // console.log(k);
+  const v  = obj3[k];                               // k는 string 타입이지만 obj의 키 타입은 one,two,three일 뿐이므로 오류 발생 Error
+}
+
+interface ABC {
+  a:string;
+  b:string;
+  c:number;
+  d:undefined
+}
+
+
+function foo1(abc:ABC){                                         // 방법1 keyof 사용
+  let k : keyof ABC; 
+  for(k in abc){
+    const v = abc[k];
+  }
+}
+
+function foo2(abc:ABC){                                        // 방법2 Object.entries() 사용
+  for(const [k,v] of Object.entries(abc)){
+    k
+    v
+  }
+}
+
+const x23 ={ a:'a', b:'b', c:2, d:undefined};
+foo1(x23);
